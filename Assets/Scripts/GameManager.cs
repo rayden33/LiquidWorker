d6bccae5cb9f9b2de26b0a387c6eae7344cb604c
Doniyorjon <rayden33@mail.ru>
2020-01-30 00:16:01 +0500

    upload_project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public LineRenderer _trajectoryLineRenderer;
+    public static GameManager instance = null;
+
+    [SerializeField] private float _maxMoveSpeed = 1f;
+    private float _moveSpeed;                           
+    public float MoveSpeed
+    {
+        get { return _moveSpeed; }
+    }
+
+    public void DrawTrajectoryLine(Vector2 _workerPosition, List<Vector3> _nextPositions)
+    {
+        if (_trajectoryLineRenderer == null)
+            _trajectoryLineRenderer = gameObject.AddComponent<LineRenderer>();
+
+        _trajectoryLineRenderer.positionCount = _nextPositions.Count + 1;
+        _trajectoryLineRenderer.SetPosition(0, _workerPosition);
+
+        for (int i = 0; i < _nextPositions.Count; i++)
+        {
+                _trajectoryLineRenderer.SetPosition(i + 1, (Vector2) _nextPositions[i]);
+        }
+    }
+
+    public void ChangeSpeed(float _sliderValue)
+    {
+        _moveSpeed = _maxMoveSpeed * _sliderValue;
+    }
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+}
---
+++ Assets/Scripts/WorkerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class WorkerController : MonoBehaviour
+{
+    private List<Vector3> _nextPositions = new List<Vector3>();
+    private Vector2 _moveDirection = new Vector2();             
+    private float _distanceOffset = 0.3f;                       // if distance between worker and next position smaller than this value worker go to next position
+
+    private void AddNextMovePosition()
+    {
+        _nextPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        GameManager.instance.DrawTrajectoryLine(transform.position, _nextPositions);
+    }
+    private void MoveToNextPostion()
+    {
+        _moveDirection = _nextPositions[0] - transform.position;
+        transform.Translate(_moveDirection * GameManager.instance.MoveSpeed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, _nextPositions[0]) < _distanceOffset)
+        {
+                GameManager.instance.DrawTrajectoryLine(transform.position, _nextPositions);
+                _nextPositions.RemoveAt(0);
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            AddNextMovePosition();
+
+        if (_nextPositions.Count > 0)
+            MoveToNextPostion();
+    }
+
+}
